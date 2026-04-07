f3b9ed0c422a1b1f9da70a9b2b21f2fd0683e91e
Emanuel <ep712122@gmail.com>
2026-04-06 18:18:34 -0600

    Init Repo

---
+++ Inicio/Inicio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Inicio
+//
+//  Created by Emanuel Altuzar on 06/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Inicio/Inicio/InicioApp.swift
@@ -0,0 +1,17 @@
+//
+//  InicioApp.swift
+//  Inicio
+//
+//  Created by Emanuel Altuzar on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct InicioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
