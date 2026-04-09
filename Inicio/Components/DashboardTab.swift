456c2276d23c8e8cd7bc5404de8684f01eb839f1
Emanuel <ep712122@gmail.com>
2026-04-09 09:30:32 -0600

    User

---
+++ Inicio/Components/DashboardTab.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardTab.swift
+//  Inicio
+//
+//  Created by Emanuel Altuzar on 08/04/26.
+//
+
+enum DashboardTab {
+    case home
+    case progress
+    case pro
+    case profile
+}
